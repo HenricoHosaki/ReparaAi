1dea9aa39802f849e905d130489f6d808a6934ee
HenricoHosaki <henricohosaki@gmail.com>
2025-10-03 08:54:40 -0300

    feat: add comments in relations.js

--- backend/src/models/relations.js
+++ backend/src/models/relations.js
@@ -4,6 +4,10 @@ const TicketTrack = require('./ticketTrackModel');
 
 /**
  * Relate the tables User and Tickets (1:N)
+ * A user can have many tickets, but a ticket belongs to one user only
+ *
+ * Relate the tables Tickets and TicketsTracks (1:N)
+ * A ticket can have many tracks, but a track belongs to one ticket only
  */
 User.hasMany(Ticket, {foreignKey: 'idUser', as: 'tickets'})
 Ticket.belongsTo(User, {foreignKey: 'idUser', as: 'user'})
